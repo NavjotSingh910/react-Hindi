54565b9d9b56b6eae4f08ca05294e5803c32e82d
navjot sandhu <navjotsandhu910@outlook.com>
2023-12-16 11:06:24 +0530

    basic custom react cover but under stand by using vite is pending

--- 02_customReact/customReact.js
+++ 02_customReact/customReact.js
@@ -8,10 +8,20 @@ const reactElement= {
 
 }
 function customRender(reactElement,container){
-    const domElement=document.createElement(reactElement.type)
+    // const domElement=document.createElement(reactElement.type)
+    // domElement.innerHTML=reactElement.children;
+    // domElement.setAttribute('href',reactElement.props.href)
+    // domElement.setAttribute('target',reactElement.props.target)
+    // container.appendChild(domElement);
+
+    const domElement = document.createElement(reactElement.type)
     domElement.innerHTML=reactElement.children;
-    domElement.setAttribute('href',reactElement.props.href)
-    domElement.setAttribute('target',reactElement.props.target)
+    for (let prop in reactElement.props) {
+        if (prop=='children'){
+            continue
+        }
+        domElement.setAttribute(prop,reactElement.props[prop] )
+    }
     container.appendChild(domElement);
 }
 
