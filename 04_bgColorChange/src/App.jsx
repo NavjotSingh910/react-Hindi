bd2aff797786ce7d9bd3f9533b3a880ab946bb16
Navjot <navjotsandhu910@outlook.com>
2023-12-18 18:17:50 +0530

    background color change project start

---
+++ 04_bgColorChange/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  return (
+    <dev className="w-full h-screen duration-200" 
+    style={{backgroun}}
+    >
+      
+    </dev>
+  )
+}
+
+export default App
